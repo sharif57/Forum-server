8e2d074de3df8b778e4cb5aac6d9cb0a75004331
sharif57 <sharifmahamud577951@gmail.com>
2024-06-02 17:23:38 +0600

    data get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,62 @@ async function run() {
 
 
     const postCollection = client.db('forum').collection('post')
+    const userCollection = client.db('forum').collection('users')
+    const commentCollection = client.db('forum').collection('comment')
+    const announcementCollection = client.db('forum').collection('announcement')
+
+
+
+
+    //announcement related
+    app.post('/announcement', async (req, res) => {
+      const newPost = req.body;
+      console.log(newPost)
+      const result = await announcementCollection.insertOne(newPost)
+      res.send(result)
+    })
+    app.get('/announcement', async (req, res) => {
+      const cursor = announcementCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    //comment related
+    app.post('/comment', async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser)
+      const result = await commentCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+    app.get('/comment/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await commentCollection.findOne(query);
+      res.send(result)
+    })
+
+    // app.get('/comment', async (req, res) => {
+    //   const cursor = commentCollection.find()
+    //   const result = await cursor.toArray()
+    //   res.send(result)
+    // })
+
+
+    //user related api
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exits', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+
 
     app.get('/post', async (req, res) => {
       const cursor = postCollection.find()
@@ -44,17 +100,17 @@ async function run() {
       res.send(result)
     })
 
-    // app.get('/post', async(req, res)=>{
-    //   console.log(req.query.authorEmail);
-    //   if(req.query?.authorEmail){
-    //     query ={authorEmail: req.query.authorEmail}
-    //   }
-    //   const result = await postCollection.find(query).toArray()
-    //   res.send(result)
-    // })
     app.get('/post/:email', async (req, res) => {
       console.log(req.params.email);
       const result = await postCollection.find({ email: req.params.email }).toArray()
+      res.send(result)
+    })
+
+    app.get('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      // console.log(re);
+      const query = { _id: new ObjectId(id) }
+      const result = await postCollection.findOne(query);
       res.send(result)
     })
 
